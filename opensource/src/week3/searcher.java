d031b07cb6db5f045ef17235a54244584a20c057
날다람쥐 <tyoon97@gmail.com>
2021-04-18 05:44:16 +0900

    최종 6주차 실습

--- opensource/src/week3/searcher.java
+++ opensource/src/week3/searcher.java
@@ -41,20 +41,12 @@ public class searcher {
 		//System.out.println(Arrays.toString(num)); //해당 형태소 갯수
 		//System.out.println();
 		int size = kl.size();
-
-		Calc(addr,size,arr,num);
-
+		InnerProduct(addr,size,arr,num);
 		CalcSim(addr,size,arr,num);
-
 
 	}
 
 	//계산하는 메소드
-
-	public void Calc(String addr, int size,String[] arr, int[]num) throws IOException, ClassNotFoundException
-	{
-	}
-
 	public void CalcSim(String addr, int size,String[] arr, int[]num) throws IOException, ClassNotFoundException
 	{
 		FileInputStream fileStream = new FileInputStream(addr);
@@ -112,10 +104,10 @@ public class searcher {
 			cosine1[i]=Math.sqrt(cosine1[i]);
 			cosine2[i]=Math.sqrt(cosine2[i]);
 			total[i] = total[i]/cosine1[i]/cosine2[i];
-
+			
 		}
-
-
+		
+		
 		//System.out.println("total[0] : "+Math.round(total[0]*100)/100.0);
 		//System.out.println("total[1] : "+Math.round(total[1]*100)/100.0);
 		//System.out.println("total[2] : "+Math.round(total[2]*100)/100.0);
@@ -138,7 +130,7 @@ public class searcher {
 			}
 		}
 		//System.out.println(Arrays.toString(name));
-
+		
 		String[] max = new String[5];
 		String tmp1;
 		double tmp2;
@@ -151,7 +143,7 @@ public class searcher {
 					tmp1 = name[j];
 					name[j] = name [i];
 					name[i]= tmp1;
-
+					
 					tmp2 = total[j];
 					total[j] = total [i];
 					total[i]= tmp2;
@@ -164,5 +156,96 @@ public class searcher {
 		System.out.println("1등은 : "+name[0]);
 		System.out.println("2등은 : "+name[1]);
 		System.out.println("3등은 : "+name[2]);	
+	}
+
+	//계산하는 메소드
+	public void InnerProduct(String addr, int size,String[] arr, int[]num) throws IOException, ClassNotFoundException
+	{
+		FileInputStream fileStream = new FileInputStream(addr);
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileStream);
+		Object object = objectInputStream.readObject();
+		objectInputStream.close();
+
+		//System.out.println("읽어온 객체의 type -> "+ object.getClass());
+
+		HashMap hashMap = (HashMap)object;
+		double[] total = new double[5];
+		for(int i=0;i<size;i++)
+		{
+			double[] value = (double[])hashMap.get(arr[i]);
+			if(value == null) {
+				//System.out.println("해쉬맵에 존재하지 않습니다. -> "+ arr[i]);
+			}else {
+				//System.out.println("해쉬맵에 존재 -------------------------------> "+ arr[i]);
+				//System.out.println(Arrays.toString(value));
+				for(int k=0;k<value.length;k++){
+					{
+						if(value[k]==0.0) {
+							total[0]=total[0]+(value[k+1]*num[i]);
+						}else if(value[k]==1.0) {
+							total[1]=total[1]+(value[k+1]*num[i]);
+						}else if(value[k]==2.0) {
+							total[2]=total[2]+(value[k+1]*num[i]);
+						}else if(value[k]==3.0) {
+							total[3]=total[3]+(value[k+1]*num[i]);
+						}else if(value[k]==4.0) {
+							total[4]=total[4]+(value[k+1]*num[i]);
+						}else
+						{
+							System.out.println("에러 발생");
+						}
+						k++;//2씩 증가
+					}
+				}
+			}
+		}
+		//System.out.println("total[0] : "+Math.round(total[0]*100)/100.0);
+		//System.out.println("total[1] : "+Math.round(total[1]*100)/100.0);
+		//System.out.println("total[2] : "+Math.round(total[2]*100)/100.0);
+		//System.out.println("total[3] : "+Math.round(total[3]*100)/100.0);
+		//System.out.println("total[4] : "+Math.round(total[4]*100)/100.0);
+
+		String[] name = new String[5];
+		int n=0;
+		String address = addr + "/../collection.xml"; 
+		//System.out.println(address);
+		File file = new File(address);//검색할 디렉토리
+		BufferedReader File = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF8"));
+		String Line = null; 
+		while( (Line = File.readLine()) != null ) {
+			if(Line.indexOf("<title>") != -1) {
+				String[] split1 = Line.split("<title>");
+				String[] split2 = split1[1].split("</title>");
+				name[n]=split2[0];
+				n++;
+			}
+		}
+		//System.out.println(Arrays.toString(name));
+		
+		String[] max = new String[5];
+		String tmp1;
+		double tmp2;
+		for(int i=0;i<total.length;i++)
+		{
+			for(int j=i+1;j<total.length;j++)
+			{
+				if(total[j]>total[i])
+				{
+					tmp1 = name[j];
+					name[j] = name [i];
+					name[i]= tmp1;
+					
+					tmp2 = total[j];
+					total[j] = total [i];
+					total[i]= tmp2;
+				}
+			}
+		}
+		//System.out.println(Arrays.toString(name));
+		//System.out.println(Arrays.toString(total));
+		System.out.println();
+		System.out.println("1등은 : "+name[0]);
+		System.out.println("2등은 : "+name[1]);
+		System.out.println("3등은 : "+name[2]);
 	}
 }
